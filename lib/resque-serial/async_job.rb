1898b25aefed9a06dafc53c04736f9458db72427
Max Kazarin <maxkazargm@gmail.com>
2013-05-20 10:45:20 +0400

    fix params for async job

--- lib/resque-serial/async_job.rb
+++ lib/resque-serial/async_job.rb
@@ -7,11 +7,24 @@ module ResqueSerial
 
       # Add a job to queue. Queue name is a class module name
       def create(target, queue, *args)
-        Resque.enqueue self, target, queue, *args
+        Resque.enqueue self, enqueue_payload(target, queue, *args)
       end
 
-      def perform(queue)
-        options = dequeue_payload queue
+      def enqueue_payload(target, queue, *args)
+        method = args.shift()
+        options = {
+            class: target.class.to_s,
+            id: target.id.to_s,
+            method: method,
+            args: args,
+        }
+        options[:scope] = target.scope.id.to_s if target.scope
+
+        options
+      end
+
+      def perform(options)
+        options.symbolize_keys!
         model = options[:class].constantize.unscoped.find(options[:id])
         model.scope = options.delete(:scope)
         model.send options[:method], *options[:args]
